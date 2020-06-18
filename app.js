2c401a74499ca0d92c6b7eb29f31e67a61692b0d
ptoninato <pmtoninato@gmail.com>
2020-06-18 15:46:41 -0500

    initial commit - express, eslint, nodemon

---
+++ app.js
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Welcome to my Nodemon API!');
+});
+
+app.listen(port, () => {
+  console.log(`Running on port ${port}`);
+});
