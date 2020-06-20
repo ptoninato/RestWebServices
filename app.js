05f59741dc5fb7627207e2c5aadad138122af33b
ptoninato <pmtoninato@gmail.com>
2020-06-19 21:06:28 -0500

    add post and save

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import bodyParser from 'body-parser'
 import Book from './models/bookModel.js';
 
 const app = express();
@@ -7,7 +8,15 @@ const db = mongoose.connect('mongodb://localhost/bookApi');
 const bookRouter = express.Router();
 const port = process.env.PORT || 3000;
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 bookRouter.route('/books')
+  .post((req, res) => {
+    const book = new Book(req.body);
+    book.save();
+    return res.status(201).json(book);
+  })
   .get((req, res) => {
     const query = {};
     if (req.query.genre) {
