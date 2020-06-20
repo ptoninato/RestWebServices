697ea27509ef858a3d7d78de73cb5fbd75f2c461
ptoninato <pmtoninato@gmail.com>
2020-06-19 21:26:00 -0500

    cleanup

--- app.js
+++ app.js
@@ -1,44 +1,16 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import bodyParser from 'body-parser'
+import bodyParser from 'body-parser';
+import bookRouterImport from './routes/bookRouter.js';
 import Book from './models/bookModel.js';
 
 const app = express();
 const db = mongoose.connect('mongodb://localhost/bookApi');
-const bookRouter = express.Router();
 const port = process.env.PORT || 3000;
+const bookRouter = bookRouterImport(Book);
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
-bookRouter.route('/books')
-  .post((req, res) => {
-    const book = new Book(req.body);
-    book.save();
-    return res.status(201).json(book);
-  })
-  .get((req, res) => {
-    const query = {};
-    if (req.query.genre) {
-      query.genre = req.query.genre;
-    }
-    Book.find(query, (err, books) => {
-      if (err) {
-        return res.send(err);
-      }
-      return res.json(books);
-    });
-  });
-
-bookRouter.route('/books/:bookId')
-  .get((req, res) => {
-    Book.findOne(req.param.bookId, (err, books) => {
-      if (err) {
-        return res.send(err);
-      }
-      return res.json(books);
-    });
-  });
 
 app.use('/api', bookRouter);
 
